c7294de764b7e49ce17b5f2d26d7d3fd33c49236
chunkind <chunkind88@gmail.com>
2017-11-15 16:15:39 +0900

    인터페이스의 도입

---
+++ toby_01_03_02/src/springbook/user/dao/UserDao.java
@@ -0,0 +1,90 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import springbook.user.domain.User;
+
+public class UserDao{
+
+	//old
+//	private SimpleConnectionMaker simpleConnectionMaker;
+	//new
+	private ConnectionMaker connectionMaker;
+	
+	public UserDao() {
+		//old
+//		simpleConnectionMaker = new SimpleConnectionMaker();
+		//new
+		connectionMaker = new DConnectionMaker();
+	}
+	
+	
+	public void add(User user) throws ClassNotFoundException, SQLException{
+		
+		//old
+//		Connection c = simpleConnectionMaker.makeNewConnection();
+		//new
+		Connection c = connectionMaker.makeConnection();
+		
+		PreparedStatement ps = c.prepareStatement("INSERT INTO USERS(ID, NAME, PASSWORD) VALUES(?, ?, ?)");
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+		
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+		
+	}
+	
+	
+	public User get(String id) throws ClassNotFoundException, SQLException{
+		
+		//old
+//		Connection c = simpleConnectionMaker.makeNewConnection();
+		//new
+		Connection c = connectionMaker.makeConnection();
+		
+		PreparedStatement ps = c.prepareStatement("SELECT * FROM USERS WHERE ID = ?");
+		ps.setString(1, id);
+		
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return user;
+		
+	}
+	
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		UserDao dao = new UserDao();
+		
+		User user = new User();
+		user.setId("chunkind");
+		user.setName("김준성");
+		user.setPassword("wnstjd88");
+		
+		dao.add(user);
+		
+		System.out.println(user.getId() + " 등록 성공");
+		User user2 = dao.get(user.getId());
+		
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		System.out.println(user2.getId() + " 조회 성공");
+		
+	}
+}
