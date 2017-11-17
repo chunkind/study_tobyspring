ee16070202f559e4c2d024f601a36d862d5f22e0
chunkind <chunkind88@gmail.com>
2017-11-17 20:36:24 +0900

    테스트 결과의 일관성

---
+++ toby_02_03_02/src/springbook/user/dao/UserDaoTest.java
@@ -0,0 +1,49 @@
+package springbook.user.dao;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import springbook.user.domain.User;
+
+public class UserDaoTest {
+	
+	@Test
+	public void addAndGet() throws ClassNotFoundException, SQLException {
+
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		
+		//new
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		User user = new User();
+		user.setId("chunkind");
+		user.setName("김준성");
+		user.setPassword("wnstjd88");
+		
+		dao.add(user);
+		//old
+//		System.out.println(user.getId() + " 등록 성공");
+		//new
+		assertThat(dao.getCount(), is(1));
+		
+		User user2 = dao.get(user.getId());
+		
+		assertThat(user2.getName(), is(user.getName()));
+		assertThat(user2.getPassword(), is(user.getPassword()));
+	}
+	
+	//old
+//	public static void main(String[] args) {
+//		JUnitCore.main("springbook.user.dao.UserDaoTest");
+//	}
+}
