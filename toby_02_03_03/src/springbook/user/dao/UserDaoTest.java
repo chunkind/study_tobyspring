d4b36b5ed52c76f106e5659e1871a1ebe3766da0
chunkind <chunkind88@gmail.com>
2017-11-20 14:31:28 +0900

    포괄적인 테스트.

---
+++ toby_02_03_03/src/springbook/user/dao/UserDaoTest.java
@@ -0,0 +1,95 @@
+package springbook.user.dao;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import springbook.user.domain.User;
+
+public class UserDaoTest {
+	
+	@Test
+	public void addAndGet() throws ClassNotFoundException, SQLException {
+
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		
+		//new
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		//old
+//		User user = new User();
+//		user.setId("chunkind");
+//		user.setName("±Ë¡ÿº∫");
+//		user.setPassword("wnstjd88");
+		//new
+		User user1 = new User("chunkind", "±Ë¡ÿº∫", "wnstjd88");
+		User user2 = new User("momo12", "π⁄¡§«œ", "wjdgk93");
+		
+		dao.add(user1);
+		assertThat(dao.getCount(), is(1));
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		//old
+//		System.out.println(user1.getId() + " µÓ∑œ º∫∞¯");
+		//new
+		
+		//new
+		User userget1 = dao.get(user1.getId());
+		assertThat(userget1.getName(), is(user1.getName()));
+		assertThat(userget1.getPassword(), is(user1.getPassword()));
+		
+		//new
+		User userget2 = dao.get(user2.getId());
+		assertThat(userget2.getName(), is(user2.getName()));
+		assertThat(userget2.getPassword(), is(user2.getPassword()));
+	}
+	
+	//new
+	public void count() throws SQLException, ClassNotFoundException{
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		User user1 = new User("momo02", "π⁄¡§«œ", "wjdgk93");
+		User user2 = new User("ksb3145", "±Ëªı∫Ω", "toqha91");
+		User user3 = new User("minjea899", "√÷πŒ¿Á", "alswo89");
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.add(user1);
+		assertThat(dao.getCount(), is(1));
+		
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		
+		dao.add(user3);
+		assertThat(dao.getCount(), is(3));
+		
+		
+	}
+	
+	@Test(expected=EmptyResultDataAccessException.class)
+	public void getUserFailure() throws SQLException, ClassNotFoundException{
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.get("unknown_id");
+	}
+	
+	//old
+//	public static void main(String[] args) {
+//		JUnitCore.main("springbook.user.dao.UserDaoTest");
+//	}
+}
