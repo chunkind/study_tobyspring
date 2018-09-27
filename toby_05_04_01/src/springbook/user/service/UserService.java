f02594903ed22ddffa526c598778f8917116d1d1
chunkind <chunkind88@gmail.com>
2018-09-27 18:03:40 +0900

    SMTP 적용

---
+++ toby_05_04_01/src/springbook/user/service/UserService.java
@@ -0,0 +1,135 @@
+package springbook.user.service;
+
+import java.io.UnsupportedEncodingException;
+import java.sql.Connection;
+import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import com.mysql.jdbc.interceptors.SessionAssociationInterceptor;
+
+import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
+
+public class UserService {
+	public static final int MIN_LOGOUNT_FOR_SILVER = 50;
+	public static final int MIN_RECCOMEND_FOR_GOLD = 30;
+	
+	UserDao userDao;
+	
+	private PlatformTransactionManager transactionManager;
+	
+	
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+	
+	public void setTransactionManager(PlatformTransactionManager transactionManager){
+		this.transactionManager = transactionManager;
+	}
+	
+	public void upgradeLevels(){
+		TransactionStatus status = 
+			this.transactionManager.getTransaction(new DefaultTransactionDefinition());
+		
+		try{
+			List<User> users = userDao.getAll();
+			for(User user : users) {
+				if(canUpgradeLevel(user)) {
+					upgradeLevel(user);
+				}
+			}
+			this.transactionManager.commit(status);
+		}catch(RuntimeException e){
+			this.transactionManager.rollback(status);
+			throw e;
+		}
+	}
+	
+	private boolean canUpgradeLevel(User user){
+		Level currentLevel = user.getLevel();
+		switch (currentLevel) {
+			case BASIC: return (user.getLogin() >= MIN_LOGOUNT_FOR_SILVER);
+			case SILVER: return (user.getRecommend() >= MIN_RECCOMEND_FOR_GOLD);
+			case GOLD: return false;
+			default: throw new IllegalArgumentException("Unknown Level: " + currentLevel);
+		}
+	}
+	
+	protected void upgradeLevel(User user) {
+		user.upgradeLevel();
+		userDao.update(user);
+		//new : mail 보내는 로직 추가
+		sendUpgradeEMail(user);
+	}
+	
+	public void add(User user){
+		if(user.getLevel() == null){
+			user.setLevel(Level.BASIC);
+		}
+		userDao.add(user);
+	}
+	
+	//new : email 보내는 메서드
+	private void sendUpgradeEMail(User user) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "mail.ksug.org");
+		Session s = Session.getInstance(props, null);
+		
+		MimeMessage message = new MimeMessage(s);
+		try{
+			message.setFrom(new InternetAddress("useradmin@ksug.org"));
+			message.addRecipient(Message.RecipientType.TO
+					, new InternetAddress(user.getEmail()));
+			message.setSubject("Upgrade 안내");
+			message.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드 되었습니다.");
+			
+			Transport.send(message);
+		}catch(AddressException e){
+			throw new RuntimeException(e);
+		}catch(MessagingException e){
+			throw new RuntimeException(e);
+		}catch(UnsupportedEncodingException e){
+			throw new RuntimeException(e);
+		}
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
