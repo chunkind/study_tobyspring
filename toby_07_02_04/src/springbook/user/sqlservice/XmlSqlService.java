eae75cf645913564af720fa246b1ea1491f575ce
chunkind <chunkind88@gmail.com>
2018-10-09 19:44:16 +0900

    변화를 위한 준비 : 인터페이스 분리

--- toby_07_02_04/src/springbook/user/sqlservice/XmlSqlService.java
+++ toby_07_02_04/src/springbook/user/sqlservice/XmlSqlService.java
@@ -17,30 +17,12 @@ public class XmlSqlService implements SqlService{
 	
 	private Map<String, String> sqlMap = new HashMap<String, String>();
 	
-	//new
 	private String sqlmapFile;
 	
-	//new
 	public void setSqlmapFile(String sqlmapFile) {
 		this.sqlmapFile = sqlmapFile;
 	}
 	
-	//old
-//	public XmlSqlService() {
-//		String contextPath = Sqlmap.class.getPackage().getName();
-//		try {
-//			JAXBContext context = JAXBContext.newInstance(contextPath);
-//			Unmarshaller unmarshaller = context.createUnmarshaller();
-//			InputStream is = UserDao.class.getResourceAsStream("sqlmap.xml");
-//			Sqlmap sqlmap = (Sqlmap)unmarshaller.unmarshal(is);
-//			for(SqlType sql : sqlmap.getSql()) {
-//				sqlMap.put(sql.getKey(), sql.getValue());
-//			}
-//		}catch (JAXBException e) {
-//			throw new RuntimeException(e);
-//		}
-//	}
-	//new
 	@PostConstruct
 	public void loadSql() {
 		String contextPath = Sqlmap.class.getPackage().getName();
